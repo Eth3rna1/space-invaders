9f18bf7720ccc5452b91063e634e9f2687d3194e
Gabriel Mendieta Hernandez <gmendieta4109@gmail.com>
2024-11-14 14:32:01 -0600

    Replaced the populated() function with is_on() and is_off() functions and make a type, Coordinate which is the same as (usize, usize); to make the code more readable

--- src/engine/mod.rs
+++ src/engine/mod.rs
@@ -1,20 +1,34 @@
+/*
+    Status::Off represents a background character
+    Status::On represents a pixel char
+*/
 pub mod pool;
 pub mod art;
 use crate::constants::{BACKGROUND_CHAR, PIXEL_CHAR};
 
+
+type Coordinate = (usize, usize);
+
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub enum Status {
+    On,
+    Off
+}
+
 pub struct Engine {
     pub length: usize,
     pub width: usize,
-    matrix: Vec<Vec<char>>,
+    matrix: Vec<Vec<Status>>,
 }
 
 
 impl Engine {
     pub fn new(dimensions: (usize, usize)) -> Self {
         let (width, length) = dimensions;
-        let mut matrix: Vec<Vec<char>> = Vec::new();
+        let mut matrix: Vec<Vec<Status>> = Vec::new();
         (0..length)
-            .for_each(|_| matrix.push((0..width).map(|_| BACKGROUND_CHAR).collect::<Vec<char>>()));
+            .for_each(|_| matrix.push((0..width).map(|_| Status::Off).collect::<Vec<Status>>()));
         Self {
             length,
             width,
@@ -22,18 +36,21 @@ impl Engine {
         }
     }
 
+    pub fn is_on(&self, coordinate : Coordinate) -> bool {
+        self.matrix[coordinate.1][coordinate.0] == Status::On
+    }
 
-    pub fn populated(&self, coordinate : (usize, usize)) -> bool {
-        return self.matrix[coordinate.1][coordinate.0] == BACKGROUND_CHAR
+    pub fn is_off(&self, coordinate : Coordinate) -> bool {
+        self.matrix[coordinate.1][coordinate.0] == Status::Off
     }
 
 
-    pub fn spawn(&mut self, c: char, coordinate: (usize, usize)) {
-        self.matrix[coordinate.1][coordinate.0] = c;
+    pub fn spawn(&mut self, coordinate: Coordinate) {
+        self.matrix[coordinate.1][coordinate.0] = Status::On;
     }
 
 
-    pub fn swap(&mut self, c1: (usize, usize), c2: (usize, usize)) {
+    pub fn swap(&mut self, c1: Coordinate, c2: Coordinate) {
         let tmp = self.matrix[c1.1][c1.0];
         self.matrix[c1.1][c1.0] = self.matrix[c2.1][c2.0];
         self.matrix[c2.1][c2.0] = tmp;
@@ -44,16 +61,24 @@ impl Engine {
         let mut interface = String::new();
         for r in 0..self.length {
             for c in 0..self.width {
-                interface.push(self.matrix[r][c]);
+                match self.matrix[r][c] {
+                    Status::On  => interface.push(PIXEL_CHAR),
+                    Status::Off => interface.push(BACKGROUND_CHAR)
+                }
             }
-            interface.push('\n');
+            interface += "\n";
+            /*
+            if r != self.length - 1 {
+                interface += "\n";
+            }
+            */
         }
         interface
     }
 
 
-    pub fn reset(&mut self, pixel : (usize, usize)) {
+    pub fn reset(&mut self, pixel : Coordinate) {
         let (x, y) = pixel;
-        self.matrix[y][x] = BACKGROUND_CHAR;
+        self.matrix[y][x] = Status::Off;
     }
 }
