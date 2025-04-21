a48cff256b42c79a0ea943afd4b594ca34b7201c
Gabriel Mendieta Hernandez <gmendieta4109@gmail.com>
2025-04-21 18:39:49 -0500

    made the `clear()`, `refresh()`, and the `sleep()` function public in the utils.rs API

--- src/utils.rs
+++ src/utils.rs
@@ -3,17 +3,17 @@
 */
 
 /// Clears the terminal screen
-pub(crate) fn clear() {
+pub fn clear() {
     print!("\x1B[2J\x1B[1;1H");
 }
 
 /// Returns the cursor to the top-left of the screen
-pub(crate) fn refresh() {
+pub fn refresh() {
     print!("\x1B[H");
 }
 
 /// Delays any thread action
-pub(crate) fn sleep(n: f64) {
+pub fn sleep(n: f64) {
     use std::thread;
     use std::time::Duration;
 
